edae4ed0dc6c7111b5c07bc0b180a40082dd7d24
Roland-Hufnagel <roland.hufnagel@gmx.de>
2023-06-20 10:18:26 +0200

    add dynamic card

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { createCharacterCard } from "./components/card/card.js";
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
@@ -12,3 +14,13 @@ const pagination = document.querySelector('[data-js="pagination"]');
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+console.log("Hallo");
+cardContainer.append(
+  createCharacterCard(
+    "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+    "Rick Sanchez",
+    "alive",
+    "",
+    55
+  )
+);
